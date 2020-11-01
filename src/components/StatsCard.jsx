21bf8afb5b86faa11b5cc2f737bdc5dbfb3f7ca7
Kunal Pant KP <47911821+pantkunalkp@users.noreply.github.com>
2020-11-01 22:48:10 +0530

    Add files via upload

---
+++ src/components/StatsCard.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 150,
+    maxWidth: 200
+  },
+  title: {
+    fontSize: 14,
+  }
+});
+
+const StatsCard = ({title, value}) => {
+    const classes = useStyles();
+    return (
+        <Card className={classes.root}>
+            <CardContent>
+                <Typography className={classes.title} color="textSecondary" gutterBottom>
+                {title}
+                </Typography>
+                <Typography variant="h5" component="h2">
+                {value}
+                </Typography>
+                
+            </CardContent>
+        </Card>
+    )
+}
+
+export default StatsCard
